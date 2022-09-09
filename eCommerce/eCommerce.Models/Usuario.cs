d659334ff045a10d2285442667cd1b088084b76a
ELIAS RIBEIRO DA SILVA COSTA <elias.ribeiro.s@gmail.com>
2022-09-09 18:20:48 -0300

    S04E04 - Modelo Contato

--- eCommerce/eCommerce.Models/Usuario.cs
+++ eCommerce/eCommerce.Models/Usuario.cs
@@ -17,10 +17,10 @@ namespace eCommerce.Models
         public string? NomeMae { get; set; }
         public string? SituacaoCadastro { get; set; }
         public DateTimeOffset DataCadastro { get; set; }
+        public Contato? Contato { get; set; }
 
         /*
          * TODO - Vincular com as classes:
-         * - Contato
          * - EnderecoEntrega
          * - Departamento
          */
