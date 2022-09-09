eec51cb5d43839c0d7996983db144ed82d44cd50
ELIAS RIBEIRO DA SILVA COSTA <elias.ribeiro.s@gmail.com>
2022-09-09 18:13:50 -0300

    S04E03 - Classe Usuário

---
+++ eCommerce/eCommerce.Models/Usuario.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eCommerce.Models
+{
+    public class Usuario
+    {
+        public int Id { get; set; }
+        public string? Nome { get; set; }
+        public string? Email { get; set; }
+        public string? Sexo { get; set; }
+        public string? RG { get; set; }
+        public string? CPF { get; set; }
+        public string? NomeMae { get; set; }
+        public string? SituacaoCadastro { get; set; }
+        public DateTimeOffset DataCadastro { get; set; }
+
+        /*
+         * TODO - Vincular com as classes:
+         * - Contato
+         * - EnderecoEntrega
+         * - Departamento
+         */
+    }
+}
