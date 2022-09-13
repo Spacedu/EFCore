7e1367303efb2828b2c8e4c3af7ac00862a34c50
ELIAS RIBEIRO DA SILVA COSTA <elias.ribeiro.s@gmail.com>
2022-09-13 15:37:15 -0300

    S04E09 - UsuarioRepository

---
+++ eCommerce/eCommerce.API/Repositories/UsuarioRepository.cs
@@ -0,0 +1,30 @@
+﻿using eCommerce.Models;
+
+namespace eCommerce.API.Repositories
+{
+    public class UsuarioRepository : IUsuarioRepository
+    {
+        public static List<Usuario> _db = new List<Usuario>();
+        public List<Usuario> Get()
+        {
+            return _db;
+        }
+        public Usuario Get(int id)
+        {
+            return _db.Find(x => x.Id == id)!;
+        }
+        public void Add(Usuario usuario)
+        {
+            _db.Add(usuario);
+        }
+        public void Update(Usuario usuario)
+        {
+            _db.Remove(Get(usuario.Id));
+            _db.Add(usuario);
+        }
+        public void Delete(int id)
+        {
+            _db.Remove(Get(id));
+        }
+    }
+}
