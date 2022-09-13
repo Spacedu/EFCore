1c3933c7a2a0b278b26345edb605e5844b0a26f9
ELIAS RIBEIRO DA SILVA COSTA <elias.ribeiro.s@gmail.com>
2022-09-13 15:25:10 -0300

    S04E08 - IUsuarioRepository

---
+++ eCommerce/eCommerce.API/Repositories/IUsuarioRepository.cs
@@ -0,0 +1,16 @@
+﻿using eCommerce.Models;
+
+namespace eCommerce.API.Repositories
+{
+    public interface IUsuarioRepository
+    {
+        /*
+         * CRUD - Create, Read/Retrieve, Update e Delete.
+         */
+        List<Usuario> Get();
+        Usuario Get(int id);
+        void Add(Usuario usuario);
+        void Update(Usuario usuario);
+        void Delete(int id);
+    }
+}
