21f635c677f34ff390df9918c4d7c144da6ff51e
ELIAS RIBEIRO DA SILVA COSTA <elias.ribeiro.s@gmail.com>
2022-09-14 11:51:53 -0300

    S04E13 - UsuariosController

---
+++ eCommerce/eCommerce.API/Controllers/UsuariosController.cs
@@ -0,0 +1,60 @@
+﻿using eCommerce.API.Repositories;
+using eCommerce.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace eCommerce.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsuariosController : ControllerBase
+    {
+        private readonly IUsuarioRepository _repository;
+
+        public UsuariosController(IUsuarioRepository repository)
+        {
+            _repository = repository;
+        }
+
+        // {endereco_site}/api/usuarios
+        [HttpGet]
+        public IActionResult Get()
+        {
+            var listaUsuarios = _repository.Get();
+
+            return Ok(listaUsuarios);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult Get(int id)
+        {
+            var usuario = _repository.Get(id);
+            if (usuario == null)
+                return NotFound("Não encontrado!");
+
+            return Ok(usuario);
+        }
+
+        [HttpPost]
+        public IActionResult Add([FromBody] Usuario usuario)
+        {
+            _repository.Add(usuario);
+            return Ok(usuario);
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult Update([FromBody]Usuario usuario, int id)
+        {
+            _repository.Update(usuario);
+
+            return Ok(usuario);
+        }
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            _repository.Delete(id);
+
+            return Ok();
+        }
+    }
+}
