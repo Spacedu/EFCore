f83c9239aa2359574f60b58455e40561c8d4ae4e
ELIAS RIBEIRO DA SILVA COSTA <elias.ribeiro.s@gmail.com>
2022-09-14 15:19:44 -0300

    S05E03 - eCommerce Context e Conexão Parte 1

---
+++ eCommerce/eCommerce.API/Database/eCommerceContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace eCommerce.API.Database
+{
+    public class eCommerceContext : DbContext
+    {
+        /*
+         * Conexão sem distinção de ambientes de execução
+         */
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=eCommerce;Integrated Security=True;");
+        }
+    }
+}
