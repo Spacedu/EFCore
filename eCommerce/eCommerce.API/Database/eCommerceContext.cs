797db83cf0d3a791e81983a85e46bff94f86a63a
ELIAS RIBEIRO DA SILVA COSTA <elias.ribeiro.s@gmail.com>
2022-09-15 17:44:42 -0300

    S06E05 - Carga inicial com EF Core Migration - Seeds

--- eCommerce/eCommerce.API/Database/eCommerceContext.cs
+++ eCommerce/eCommerce.API/Database/eCommerceContext.cs
@@ -15,6 +15,18 @@ namespace eCommerce.API.Database
         public DbSet<EnderecoEntrega> EnderecosEntrega { get; set; }
         public DbSet<Departamento> Departamentos { get; set; }
 
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Departamento>().HasData(
+                new Departamento { Id = 1, Nome = "Mercado" },
+                new Departamento { Id = 2, Nome = "Moda" },
+                new Departamento { Id = 3, Nome = "Móveis" },
+                new Departamento { Id = 4, Nome = "Informática" },
+                new Departamento { Id = 5, Nome = "Eletrodomésticos" },
+                new Departamento { Id = 6, Nome = "Eletroportáteis" },
+                new Departamento { Id = 7, Nome = "Beleza" }
+            );
+        }
 
         #region Conexão sem distinção de ambientes de execução
         /*
---
+++ eCommerce/eCommerce.API/Migrations/20220915204204_Carga01.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace eCommerce.API.Migrations
+{
+    public partial class Carga01 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Departamentos",
+                columns: new[] { "Id", "Nome" },
+                values: new object[,]
+                {
+                    { 1, "Mercado" },
+                    { 2, "Moda" },
+                    { 3, "Móveis" },
+                    { 4, "Informática" },
+                    { 5, "Eletrodomésticos" },
+                    { 6, "Eletroportáteis" },
+                    { 7, "Beleza" }
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Departamentos",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Departamentos",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Departamentos",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Departamentos",
+                keyColumn: "Id",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "Departamentos",
+                keyColumn: "Id",
+                keyValue: 5);
+
+            migrationBuilder.DeleteData(
+                table: "Departamentos",
+                keyColumn: "Id",
+                keyValue: 6);
+
+            migrationBuilder.DeleteData(
+                table: "Departamentos",
+                keyColumn: "Id",
+                keyValue: 7);
+        }
+    }
+}
