5923d0b9ca9f75be1d39e11cc243d6be7f90f867
ELIAS RIBEIRO DA SILVA COSTA <elias.ribeiro.s@gmail.com>
2022-09-16 16:39:01 -0300

    S07E09 - Criado projeto para FluentAPI

---
+++ eCommerce/eCommerce.Models.FluentAPI/Models/EnderecoEntrega.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eCommerce.Models
+{
+    public class EnderecoEntrega
+    {
+        public int Id { get; set; }
+        public int UsuarioId { get; set; }
+        public string NomeEndereco { get; set; } = null!;
+        public string CEP { get; set; } = null!;
+        public string Estado { get; set; } = null!;
+        public string Cidade { get; set; } = null!;
+        public string Bairro { get; set; } = null!;
+        public string Endereco { get; set; } = null!;
+        public string? Numero { get; set; }
+        public string? Complemento { get; set; }
+        public Usuario? Usuario { get; set; }
+    }
+}
---
+++ eCommerce/eCommerce.Models.FluentAPI/eCommerceFluentContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eCommerce.Models.FluentAPI
+{
+    public class eCommerceFluentContext : DbContext
+    {
+        public eCommerceFluentContext(DbContextOptions<eCommerceFluentContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Usuario>? Usuarios { get; set; }
+        public DbSet<Contato>? Contatos { get; set; }
+        public DbSet<EnderecoEntrega>? EnderecosEntrega { get; set; }
+        public DbSet<Departamento>? Departamentos { get; set; }
+    }
+}
