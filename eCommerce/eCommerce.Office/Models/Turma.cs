9c7bae35edf622f072e88f9679aba9603c4c952c
ELIAS RIBEIRO DA SILVA COSTA <elias.ribeiro.s@gmail.com>
2022-09-20 16:34:57 -0300

    S08E01 - Criando o Projeto

---
+++ eCommerce/eCommerce.Office/Models/Turma.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eCommerce.Office.Models
+{
+    public class Turma
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; } = null!;
+        public ICollection<Colaborador>? Colaboradores { get; set; }
+    }
+}
---
+++ eCommerce/eCommerce.Office/Models/Veiculo.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eCommerce.Office.Models
+{
+    public class Veiculo
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; } = null!; 
+        public string Placa { get; set; } = null!;
+
+        public ICollection<Colaborador>? Colaboradores { get; set; }
+    }
+}
