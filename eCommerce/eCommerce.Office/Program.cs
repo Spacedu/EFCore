013b7a445a55e4ac265e4487b30784f37114d57f
ELIAS RIBEIRO DA SILVA COSTA <elias.ribeiro.s@gmail.com>
2022-09-20 18:07:51 -0300

    S08E05 - Consultas SQL com ThenInclude

--- eCommerce/eCommerce.Office/Program.cs
+++ eCommerce/eCommerce.Office/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using eCommerce.Office;
+using Microsoft.EntityFrameworkCore;
+
+var db = new eCommerceOfficeContext();
+
+#region Many-To-Many > 2x One-To-Many = EF Core < 5.0 (3.1....1.0)
+//Setor > ColaboradoresSetores > Colaborador
+var resultado = db.Setores!.Include(a=>a.ColaboradoresSetores).ThenInclude(a=>a.Colaborador);
+foreach (var setor in resultado)
+{
+    Console.WriteLine(setor.Nome);
+    foreach(var colabSetor in setor.ColaboradoresSetores)
+    {
+        Console.WriteLine(" - " + colabSetor.Colaborador.Nome);
+    }
+}
+#endregion
