318b05e51735c9179e5cc454d452dfd6ef456dd3
ELIAS RIBEIRO DA SILVA COSTA <elias.ribeiro.s@gmail.com>
2022-09-20 16:46:42 -0300

    S08E02 - Classe de Contexto

--- eCommerce/eCommerce.Office/eCommerceOfficeContext.cs
+++ eCommerce/eCommerce.Office/eCommerceOfficeContext.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using eCommerce.Office.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,16 @@ using System.Threading.Tasks;
 
 namespace eCommerce.Office
 {
-    internal class eCommerceOfficeContext
+    public class eCommerceOfficeContext : DbContext
     {
+        public DbSet<Colaborador>? Colaboradores { get; set; }
+        public DbSet<Setor>? Setores { get; set; }
+        public DbSet<Turma>? Turmas { get; set; }
+        public DbSet<Veiculo>? Veiculos { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=eCommerceOffice;Integrated Security=True;");
+        }
     }
 }
