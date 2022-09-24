55a00c0812de4365b8d2857ae8f19d8e95c53126
ELIAS RIBEIRO DA SILVA COSTA <elias.ribeiro.s@gmail.com>
2022-09-24 11:56:16 -0300

    S10E02 - Criando Projeto

---
+++ eCommerce/eCommerce.Console.Tracking/Models/Contato.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eCommerce.Models
+{
+    public class Contato
+    {
+        public int Id { get; set; }
+        public int UsuarioId { get; set; }
+        public string? Telefone { get; set; }
+        public string? Celular { get; set; }
+
+        public Usuario? Usuario { get; set; }
+    }
+}
