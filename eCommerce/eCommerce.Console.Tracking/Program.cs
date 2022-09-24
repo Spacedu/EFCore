364dd47d8022f3ce2219dfe4135d3066bcf6e0b6
ELIAS RIBEIRO DA SILVA COSTA <elias.ribeiro.s@gmail.com>
2022-09-24 13:08:49 -0300

    S10E04 - Tracking e NoTracking

--- eCommerce/eCommerce.Console.Tracking/Program.cs
+++ eCommerce/eCommerce.Console.Tracking/Program.cs
@@ -1,2 +1,20 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using eCommerce.API.Database;
+using Microsoft.EntityFrameworkCore;
+
+var db = new eCommerceContext();
+
+/*
+ * var usuario01 = db.Usuarios.FirstOrDefault(a=>a.Id == 1);
+usuario01!.Nome = "José Ribeiro da Silva Costa";
+
+db.SaveChanges();
+*/
+
+var usuario01 = db.Usuarios.AsNoTracking().FirstOrDefault(a => a.Id == 1);
+usuario01!.Nome = "José Ribeiro da Silva Costa";
+
+db.Update(usuario01);
+db.SaveChanges();
+
+
