080ac6a4dd945ef30fd8a60d33f942a5e72b2990
ELIAS RIBEIRO DA SILVA COSTA <elias.ribeiro.s@gmail.com>
2022-09-29 10:58:15 -0300

    S11E01 - Criado o projeto

---
+++ eCommerce/eCommerce.Console.Query/Models/Usuario.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eCommerce.Models
+{
+    /*
+     * EFCore + Migration: 
+     * Tabela: Usuarios
+     *  Id - PK
+     *  Nome - nvarchar - not null
+     *  Email - nvarchar - not null
+     *  Sexo - nvarchar - null
+     *  CPF - nvarchar - not null
+     *  ....
+     */
+    public class Usuario
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; } = null!;
+        public string Email { get; set; } = null!;
+        public string? Sexo { get; set; }
+        public string? RG { get; set; }
+        public string CPF { get; set; } = null!;
+        public string? Mae { get; set; }
+        public string? SituacaoCadastro { get; set; }
+        public DateTimeOffset DataCadastro { get; set; }
+        public Contato? Contato { get; set; }
+        public ICollection<EnderecoEntrega>? EnderecosEntrega { get; set; }
+        public ICollection<Departamento>? Departamentos { get; set; }
+    }
+}
---
+++ eCommerce/eCommerce.Console.Query/eCommerceContext.cs
@@ -0,0 +1,17 @@
+﻿using eCommerce.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace eCommerce.API.Database
+{
+    public class eCommerceContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=eCommerce;Integrated Security=True;");
+        }
+        public DbSet<Usuario>? Usuarios { get; set; }
+        public DbSet<Contato>? Contatos { get; set; }
+        public DbSet<EnderecoEntrega>? EnderecosEntrega { get; set; }
+        public DbSet<Departamento>? Departamentos { get; set; }
+    }
+}
