dc6f38ac85cf354937b9494f952fd7706103745c
ELIAS RIBEIRO DA SILVA COSTA <elias.ribeiro.s@gmail.com>
2022-09-30 13:04:59 -0300

    S11E02 - Consultas com ToList, ToArray, ToHashSet e ToDictionary

--- eCommerce/eCommerce.Console.Query/Program.cs
+++ eCommerce/eCommerce.Console.Query/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using eCommerce.API.Database;
+
+/*
+ * EF Core > Support LINQ > SQL - EFCore > SGDB
+ * To*, First*, Single*,Last*, Count... Executa o Código SQL -> App -> C# (Memory)
+ * 
+ * db.Usuario!.{Methods > LINQ > EF > SQL > SGDB}.ToList().{Methods > LINQ > C# > Processador+Memory}; 
+ */
+
+var db = new eCommerceContext();
+var usuarios = db.Usuarios!.ToList();
+
+Console.WriteLine("LISTA DE USUÁRIOS");
+foreach (var usuario in usuarios)
+{
+    Console.WriteLine($" - {usuario.Nome}");
+}
