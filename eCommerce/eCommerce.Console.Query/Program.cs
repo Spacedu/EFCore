4321774e64407053c4cd785e6199b237c775f95e
ELIAS RIBEIRO DA SILVA COSTA <elias.ribeiro.s@gmail.com>
2022-09-30 14:32:28 -0300

    S11E04 - Single, Count, Min e Max.

--- eCommerce/eCommerce.Console.Query/Program.cs
+++ eCommerce/eCommerce.Console.Query/Program.cs
@@ -2,7 +2,7 @@
 
 /*
  * EF Core > Support LINQ > SQL - EFCore > SGDB
- * To*, Find, First*, Single*,Last*, Count... Executa o Código SQL -> App -> C# (Memory)
+ * To*, Find, First*, Single*,Last*, Count, Max, Min... Executa o Código SQL -> App -> C# (Memory)
  * 
  * db.Usuario!.{Methods > LINQ > EF > SQL > SGDB}.ToList().{Methods > LINQ > C# > Processador+Memory}; 
  */
@@ -22,9 +22,21 @@ Console.WriteLine("BUSCAR 1 USUÁRIO");
 //var usuario01 = db.Usuarios!.Where(a => a.Email == "filipe.ribeiro@gmail.com").FirstOrDefault();
 //var usuario01 = db.Usuarios!.OrderBy(a=>a.Id).Last();
 //var usuario01 = db.Usuarios!.OrderBy(a=>a.Id).Where(a => a.Email == "elias.ribeiro.s@gmail.com").LastOrDefault();
-var usuario01 = db.Usuarios!.FirstOrDefault(a => a.Id == 2);
+//var usuario01 = db.Usuarios!.FirstOrDefault(a => a.Id == 2);
+var usuario01 = db.Usuarios!.Single(a=>a.Email == "filipe.ribeiro@gmail.com");
+//var usuario01 = db.Usuarios!.SingleOrDefault(a=>a.Nome.Contains("a"));
 
 if (usuario01 == null)
     Console.WriteLine("Usuário não encontrado!");
 else
     Console.WriteLine($" COD: {usuario01!.Id} - NOME: {usuario01!.Nome}");
+
+var count = db.Usuarios!.Where(a => a.Nome.Contains("a")).Count();
+Console.WriteLine($"NÚMERO DE USUÁRIOS QUE CONTÉM A LETRA 'A' EM SEU NOME: {count}");
+
+
+var min = db.Usuarios!.Min(a => a.Nome);
+Console.WriteLine($"VALOR MIN(ID):{min}");
+
+var max = db.Usuarios!.Max(a => a.Nome);
+Console.WriteLine($"VALOR MAX(ID):{max}");
