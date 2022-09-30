9020f7de20cf661e28f94c5a0a5631190e71d184
ELIAS RIBEIRO DA SILVA COSTA <elias.ribeiro.s@gmail.com>
2022-09-30 13:35:13 -0300

    S11E03 - Find, First e Last.

--- eCommerce/eCommerce.Console.Query/Program.cs
+++ eCommerce/eCommerce.Console.Query/Program.cs
@@ -2,7 +2,7 @@
 
 /*
  * EF Core > Support LINQ > SQL - EFCore > SGDB
- * To*, First*, Single*,Last*, Count... Executa o Código SQL -> App -> C# (Memory)
+ * To*, Find, First*, Single*,Last*, Count... Executa o Código SQL -> App -> C# (Memory)
  * 
  * db.Usuario!.{Methods > LINQ > EF > SQL > SGDB}.ToList().{Methods > LINQ > C# > Processador+Memory}; 
  */
@@ -15,3 +15,16 @@ foreach (var usuario in usuarios)
 {
     Console.WriteLine($" - {usuario.Nome}");
 }
+
+Console.WriteLine("BUSCAR 1 USUÁRIO");
+//var usuario01 = db.Usuarios!.Find(2);
+//var usuario01 = db.Usuarios!.Where(a=>a.Email == "elias.ribeiro.s@gmail.com").First();
+//var usuario01 = db.Usuarios!.Where(a => a.Email == "filipe.ribeiro@gmail.com").FirstOrDefault();
+//var usuario01 = db.Usuarios!.OrderBy(a=>a.Id).Last();
+//var usuario01 = db.Usuarios!.OrderBy(a=>a.Id).Where(a => a.Email == "elias.ribeiro.s@gmail.com").LastOrDefault();
+var usuario01 = db.Usuarios!.FirstOrDefault(a => a.Id == 2);
+
+if (usuario01 == null)
+    Console.WriteLine("Usuário não encontrado!");
+else
+    Console.WriteLine($" COD: {usuario01!.Id} - NOME: {usuario01!.Nome}");
