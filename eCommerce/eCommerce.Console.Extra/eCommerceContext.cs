98cce3d1997992077adaa7e96a0c3d8a7eb861aa
ELIAS RIBEIRO DA SILVA COSTA <elias.ribeiro.s@gmail.com>
2022-10-11 18:11:46 -0300

    S12E04 - Tabelas Temporais - Parte 1 - Habilitar

--- eCommerce/eCommerce.Console.Extra/eCommerceContext.cs
+++ eCommerce/eCommerce.Console.Extra/eCommerceContext.cs
@@ -7,7 +7,7 @@ namespace eCommerce.API.Database
     {
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            optionsBuilder.UseSqlServer("Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=eCommerce;Integrated Security=True;");
+            optionsBuilder.UseSqlServer("Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=eCommerceTemp;Integrated Security=True;");
         }
         public DbSet<Usuario>? Usuarios { get; set; }
         public DbSet<Contato>? Contatos { get; set; }
@@ -16,6 +16,16 @@ namespace eCommerce.API.Database
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
+            modelBuilder.Entity<Usuario>().ToTable("Usuarios", t => t.IsTemporal(
+                b => {
+                    b.HasPeriodStart("PeriodioInicial");
+                    b.HasPeriodEnd("PeriodoFinal");
+                    b.UseHistoryTable("UsuariosHistorico");
+                }
+                ));
+
+
+
             modelBuilder.Entity<Usuario>().HasQueryFilter(a => a.SituacaoCadastro == SituacaoCadastro.Ativo);
 
             /*
